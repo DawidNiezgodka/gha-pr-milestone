49bca20d8cc8616419346d15604affa939d50bb0
Dawid Niezgódka <dawid.niezgodka@gmail.com>
2023-02-26 19:59:30 +0100

    added guards that check if an event is closed and merged PR

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-echo "my action is running"
+if [[ "$GITHUB_EVENT_NAME" != "pull_request" ]]; then
+  echo "This action only runs on pull requests."
+  echo "Found: $GITHUB_EVENT_NAME"
+  exit 1
+fi
+
+ACTION=$(jq -r .action $GITHUB_EVENT_PATH)
+if [[ "$ACTION" != "closed" ]]; then
+  echo "This action only runs when a pull request is closed."
+  echo "Found: $GITHUB_EVENT_NAME.$ACTION"
+  exit 1
+fi
+
+MERGED=$(jq -r .pull_request.merged $GITHUB_EVENT_PATH)
+if [[ "$MERGED" != "true" ]]; then
+  echo "This action only runs when a pull request is merged."
+  echo "Found: $GITHUB_EVENT_NAME.$ACTION.merged=$MERGED"
+  exit 1
+fi
 
